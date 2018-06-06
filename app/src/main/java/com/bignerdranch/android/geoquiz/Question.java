3981c7e84bd6e3b8ea4922722b456c9dfa318b48
Yisroel Weisberg <weisbeym@mail.uc.edu>
2018-06-05 22:33:11 -0400

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.geoquiz;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
